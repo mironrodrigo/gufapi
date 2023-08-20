a5a7a2ebfd2dc908352c6f853f39433657e971f0
mironrodrigo <38597606+mironrodrigo@users.noreply.github.com>
2023-08-20 17:18:33 -0300

    Update configs.py

--- core/configs.py
+++ core/configs.py
@@ -6,7 +6,7 @@ from sqlalchemy.ext.declarative import declarative_base
 
 class Settings(BaseSettings):
     API_V1_STR: str = '/api/v1'
-    DB_URL: str = 'postgresql+asyncpg://postgres:miron@localhost:5432/faculdade'
+    DB_URL: str = 'postgresql+asyncpg://miron:miron@localhost:5432/faculdade'
     DBBaseModel = declarative_base()
 
     JWT_SECRET: str = '0CBnDn1V2dpWdvNZwHym8hi2Ya0jkZHZ3A46fILIlgo'
